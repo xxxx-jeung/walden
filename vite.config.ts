d1ecf817ab7c50d4a8c2444154b95d7e971a5dc7
xxxx-jeung <63729676+xxxx-jeung@users.noreply.github.com>
2024-03-29 19:47:33 +0900

    배포

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    outDir: './docs'
+  },
   css: {
     preprocessorOptions: {
       scss: {
